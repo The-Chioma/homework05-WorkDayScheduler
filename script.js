b4e6fa23c1ef593bc3bafe3c67009cf1bbbc2372
Chioma Onyeguke <chiomaonyeguke1@gmail.com>
2020-11-21 17:55:21 +0000

    commit on main

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var m = moment();
-var today = $("#currentDay").text(m.format("LLL"));
+var today = $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
 var currentHour = moment().hour();
 var currentHourNo = parseInt(currentHour);
@@ -18,7 +18,8 @@ for (var i = 9; i < 18; i++) {
     //this div is a row
     var newDiv = $("<div>");
     newDiv.attr("class", "row hour time-block");
-    
+    newDiv.attr("id", i.toString());
+
     //this is where the time hours are displayed
     var timeHolder = $("<h4>");
     timeHolder.attr("class", "col-2");
@@ -27,7 +28,7 @@ for (var i = 9; i < 18; i++) {
     
     //this is where the text input is displayed
     var textInput = $("<input>");
-    textInput.attr("class", "col-8");
+    textInput.attr("class", "col-8 userEvent");
     newDiv.append(textInput);
     
     //save button and adding icon to button from Font Awesome
@@ -53,10 +54,29 @@ for (var i = 9; i < 18; i++) {
     if (i === currentHourNo) {
         textInput.addClass("present");
     }   
-      
-    //click listener for the save buttons
-$(".saveBtn").on("click", function(){
-   //need to figure out local storage and saving after refreshing.
-    
+}      
 
-})}
+
+    //event listener for the save buttons
+    $(".saveBtn").on("click", function(){
+     //storing the text input in local storage
+        var event = $(this).siblings(".userEvent").val();
+        console.log(event);
+
+        var time = $(this).parent().attr("id");
+        console.log(time);
+
+        localStorage.setItem(time, event);
+    });
+
+    //getting the items from local storage byt their unique IDs and class names
+    $("#9 .userEvent").val(localStorage.getItem("9"));
+    $("#10 .userEvent").val(localStorage.getItem("10"));
+    $("#11 .userEvent").val(localStorage.getItem("11"));
+    $("#12 .userEvent").val(localStorage.getItem("12"));
+    $("#13 .userEvent").val(localStorage.getItem("13"));
+    $("#14 .userEvent").val(localStorage.getItem("14"));
+    $("#15 .userEvent").val(localStorage.getItem("15"));
+    $("#16 .userEvent").val(localStorage.getItem("16"));
+    $("#17 .userEvent").val(localStorage.getItem("17"));
+ 
