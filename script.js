9542a36f2e444c1e637a7e6e9603925fa76e00d8
Chioma Onyeguke <chiomaonyeguke1@gmail.com>
2020-11-21 10:01:13 +0000

    commit on main

--- script.js
+++ script.js
@@ -12,28 +12,38 @@ var container = $(".container")
 var newDiv = $("<div>");
 newDiv.attr("class", "row hour time-block");
 
-
+//this loop will create timeblocks from 9:00 to 17:00
 for (var i = 9; i < 18; i++) {
 
+    //this div is a row
     var newDiv = $("<div>");
     newDiv.attr("class", "row hour time-block");
     
+    //this is where the time hours are displayed
     var timeHolder = $("<h4>");
     timeHolder.attr("class", "col-2");
     timeHolder.text(i + ":00");
     newDiv.append(timeHolder);
     
+    //this is where the text input is displayed
     var textInput = $("<input>");
     textInput.attr("class", "col-8");
     newDiv.append(textInput);
     
+    //save button and adding icon to button from Font Awesome
     var button = $("<button>");
     button.attr("class", "col-2 saveBtn");
-    button.text("Save");
+    
+    //icon from Font Awesome
+    var buttonImage = $("<i>");
+    buttonImage.addClass("far fa-save");
+    button.append(buttonImage);
+    
     newDiv.append(button);
     
     container.append(newDiv);
 
+    //this is to change the colour scheme according to the current, past and future time.
     if (i < currentHourNo) {
         textInput.addClass("past");
     }
@@ -44,10 +54,9 @@ for (var i = 9; i < 18; i++) {
         textInput.addClass("present");
     }   
       
-
+    //click listener for the save buttons
 $(".saveBtn").on("click", function(){
    //need to figure out local storage and saving after refreshing.
     
 
 })}
-
